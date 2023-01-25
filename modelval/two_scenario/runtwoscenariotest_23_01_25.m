a166fc28e79d7f429cc25265e30730a4856b05f8
tamdur <tamdur4@gmail.com>
2023-01-25 12:42:50 -0500

    addpath for cluster use

--- modelval/two_scenario/runtwoscenariotest_23_01_25.m
+++ modelval/two_scenario/runtwoscenariotest_23_01_25.m
@@ -6,6 +6,8 @@ clearvars
 parpool('local',str2num(getenv('SLURM_CPUS_PER_TASK')))
 %Make sure everything is visible
 addpath('/net/rcstorenfs02/ifs/rc_labs/huybers_lab/tamdur/ACRIM-Gap')
+addpath('/net/rcstorenfs02/ifs/rc_labs/huybers_lab/tamdur/ACRIM-Gap/tools')
+addpath('/net/rcstorenfs02/ifs/rc_labs/huybers_lab/tamdur/ACRIM-Gap/mat_files')
 rng(1)
 
 %Load the synthetic datasets to be examined
