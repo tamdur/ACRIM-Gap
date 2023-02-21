92e7819c166fca8df2f329bd6f8ead0475a62a97
tamdur <tamdur4@gmail.com>
2023-02-21 11:03:13 -0500

    Prepare large cluster experiment

--- modeleval/alt_runs/runalts.sh
+++ modeleval/alt_runs/runalts.sh
@@ -3,10 +3,10 @@
 #SBATCH -o runalts.out
 #SBATCH -e runalts.err
 #SBATCH -N 1
-#SBATCH -c 32
-#SBATCH -t 0-06:30
+#SBATCH -c 10
+#SBATCH -t 0-12:30
 #SBATCH -p huce_cascade
-#SBATCH --mem=64G
+#SBATCH --mem=32G
 #SBATCH --mail-type=END
 #SBATCH --mail-user=amdur@g.harvard.edu
 
